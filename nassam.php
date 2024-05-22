a87d98418dc06b7cbc05a8c4f727af075d61d8c3
divyadharshiniaiml <146611612+divyadharshiniaiml@users.noreply.github.com>
2024-05-22 14:37:58 +0530

    Add files via upload

---
+++ nassam.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+<div class="container-fluid">
+  <h1>Capital-Dispur</h1>
+  <h2>History</h2>
+  <img src="ac.jpg" class="thumbnail" alt="Dhuska" width="304" height="236">
+  <p>Dispur is the capital of the Indian state of Assam and is a suburb of Guwahati.
+  It became the capital in 1973, when Shillong the erstwhile capital, became the capital of the state of Meghalaya that was carved out of Assam.
+  Dispur is the seat of power of Government of Assam. The Assam Secretariat (Janata Bhawan) building is located in Dispur along with the Assam Legislative Assembly House, MLA Hostels and the State Emergency Operations Centre. The Assam Trunk road and the G S road pass through Dispur. To the south of Dispur lies the theologically important site of Basistha Ashram and the Shankardev Kalakshetra, a cultural centre created in the 1990s. Next to Dispur is the township of Jatia.
+  The Guwahati Tea Auction Centre, one of the busiest tea trading facilities in the world, is located in the city.</p>
+  <h1>Emblem</h1>
+  <img src="ae.jpg" class="thumbnail" alt="Assam" width="304" height="236">
+  <p>At present the state of Assam in India has not adopted a distinctive emblem for government use and instead uses the national emblem of India with the words "Oxom Sorkar" above and "Government of Assam" below.[1] In February 2022, the Government of Assam decided to form a committee to consider the design for a distinctive emblem for the state.</p>
+    <h1>Government of Punjab</h1>
+    <h2>Chief Minister</h2>
+    <img src="acm.jpg" class="thumbnail" alt="Assam" width="304" height="236">
+    <p><h3>Incumbent</h3></p>
+      <p><h3>Himanta Biswa Sarma</h3></p>
+      <p><h3>since 10 May 2021</h3></p>
+    <p><b>Style</b>	The Honourable (Formal)
+      Mr. Chief Minister (Informal)</p>
+      <p><b>Type	Head of Government</b></p>
+      <p>Status	Leader of the Executive</p>
+      <p><b>Abbreviation	CM</b></p>
+      <p><b>Member of</b>Assam Legislative Assembly</p>
+      <p><b>Reports to</b>Governor of Assam</p>
+      <p><b>Appointer</b>Governor of Assam</p>
+      <p><b>Term length</b></p>At the confidence of the assembly
+      Chief minister's term is for five years and is subject to no term limits.</p>
+      <p><b>Inaugural holder</b> holder	Gopinath Bordoloi</p>
+      <p><b>Formation</b>26 January 1950(74 years ago)</p>
+      <p><b>Deputy</b>Deputy Chief Minister of Assam</p>
+      <h2>Governor</h2>
+      <img src="ag.jpg" class="thumbnail" alt="Dhuska" width="304" height="236">
+      <p><h3>Incumbent</h3></p>
+      <p><h3>Gulab Chand Kataria</h3></p>
+      <p><h3>since 22 February 2023</h3></p>
+      <p><b>Style</b>	His/Her Excellency
+      <p><b>Residence</b>	Raj Bhavan,Guwahati</p>
+      <p><b>Term length</b>Five years</p>
+      <p><b>Inaugural holder</b>Muhammad Saleh Akbar Hydari (Independent India)Nicholas Beatson-Bell (Pre-Independent India)</p>
+      <p><b>Formation</b>3 January 1921; 103 years ago</p>      <h1>Famous Food</h1>
+       <h2>1.Baanhgajor Lagot Kukura (Chicken with Bamboo Shoot)</h2>
+       <img src="af1.jpg" class="thumbnail" alt="Dhuska" width="304" height="236">
+       <p>This is a unique dish of the food of Assam made with chicken meat, bamboo shoots and lentils. The taste of bamboo shoots is not very common and doesn’t suit everyone but once you eat it, it leaves behind a unique taste which is generally complimentary to meat dishes.</p> 
+       <h2>2.Pitha – Home cooked Sweet dishes of Assam</h2>
+       <img src="af2.jpg" class="thumbnail" alt="Dhuska" width="304" height="236">
+       <p>Pitha is one of the most popular snack time dishes, usually eaten at breakfast or with evening tea. There is a wide variety of pithas available. They can be sweet or savoury, steamed or fried, cooked in a many different techniques. This is one of the most technical foods of Assam and needs a lot of precision to get the dish to turn out correct.</p>
+
+</div>
+<a href="index.php">Go back to our official website</a>
+
+</body>
+</html>
